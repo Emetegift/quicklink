e7e5fc4607a2ad94f898bb5c7bbb8388ce2ca9bf
Emete Gift <emeteokeoghene@gmail.com>
2023-05-21 21:07:38 +0100

    Link and Users schema

--- api/schemas/link.py
+++ api/schemas/link.py
@@ -1,20 +1,20 @@
-# from marshmallow import Schema, fields
-# from ..schemas import UserSchema
+from marshmallow import Schema, fields
+from ..schemas import UserSchema
 
 
-# class LinkSchema(Schema):
-#     id = fields.Integer(dump_only=True)
-#     # user = fields.Nested(UserSchema, only=("id", "first_name", "last_name","password", "confirm_password", "username", "email"))
-#     user_id = fields.Integer(required=True)
-#     original_url = fields.String(required=True)
-#     short_url = fields.String(dump_only=True)
-#     date_created = fields.DateTime(dump_only=True)
+class LinkSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    # user = fields.Nested(UserSchema, only=("id", "first_name", "last_name","password", "confirm_password", "username", "email"))
+    user_id = fields.Integer(required=True)
+    original_url = fields.String(required=True)
+    short_url = fields.String(dump_only=True)
+    date_created = fields.DateTime(dump_only=True)
 
 
-# class GetLinks(Schema):
-#     id = fields.Integer()
-#     user_id = fields.Integer()
-#     original_url = fields.String()
-#     short_url = fields.String()
-#     visit = fields.Integer()
-#     date_created = fields.String()
+class GetLinks(Schema):
+    id = fields.Integer()
+    user_id = fields.Integer()
+    original_url = fields.String()
+    short_url = fields.String()
+    visit = fields.Integer()
+    date_created = fields.String()
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,27 +1,27 @@
-# from marshmallow import Schema, ValidationError, \
-# validates_schema, fields
+from marshmallow import Schema, ValidationError, \
+validates_schema, fields
 
 
-# class UserSchema(Schema):
-#     id = fields.Integer(dump_only=True)
-#     username = fields.String(required=True)
-#     first_name = fields.String(required=True)
-#     last_name = fields.String(required=True)
-#     email = fields.Email(required=True)
-#     password = fields.String(required=True, load_only=True)
-#     confirm_password = fields.String(load_only=True, required=True)
+class UserSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    username = fields.String(required=True)
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
+    email = fields.Email(required=True)
+    password = fields.String(required=True, load_only=True)
+    confirm_password = fields.String(load_only=True, required=True)
 
 
-# class LoginSchema(Schema):
-#     email = fields.Email(required=True)
-#     password = fields.String(required=True, load_only=True)
+class LoginSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.String(required=True, load_only=True)
 
-#     @validates_schema
-#     def validate_password(self, data, **kwargs):
-#         if not data['password']:
-#             raise ValidationError('Password is required')
+    @validates_schema
+    def validate_password(self, data, **kwargs):
+        if not data['password']:
+            raise ValidationError('Password is required')
 
-#     @validates_schema
-#     def validate_email(self, data, **kwargs):
-#         if not data['email']:
-#             raise ValidationError('Email is required')
+    @validates_schema
+    def validate_email(self, data, **kwargs):
+        if not data['email']:
+            raise ValidationError('Email is required')
