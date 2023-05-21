e301917ae58835aeb36a0c42131cbfe1efaa291a
Emete Gift <emeteokeoghene@gmail.com>
2023-05-21 21:08:40 +0100

    Validate url

--- api/utils/validate_url.py
+++ api/utils/validate_url.py
@@ -1,12 +1,12 @@
-# from urllib import request
-# from urllib.error import HTTPError, URLError
+from urllib import request
+from urllib.error import HTTPError, URLError
 
 
-# def validate_url(url):
-#     if not url.startswith('http://') and not url.startswith('https://'):
-#         url = 'http://' + url
-#     try:
-#         request.urlopen(url)
-#         return True
-#     except (HTTPError, URLError):
-#         return False
+def validate_url(url):
+    if not url.startswith('http://') and not url.startswith('https://'):
+        url = 'http://' + url
+    try:
+        request.urlopen(url)
+        return True
+    except (HTTPError, URLError):
+        return False
