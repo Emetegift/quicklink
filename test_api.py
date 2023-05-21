8e52ded7dc53043f5c5d4e14919fd5476b497d3b
Emete Gift <emeteokeoghene@gmail.com>
2023-05-21 21:13:02 +0100

    Unittest file

--- test_api.py
+++ test_api.py
@@ -1,28 +1,28 @@
-# import unittest
-# from api import create_app
-# from api.config.config import TestConfig
-# from api. extensions import db
+import unittest
+from api import create_app
+from api.config.config import TestConfig
+from api. extensions import db
 
-# class APITestCase(unittest.TestCase):
-#     def setUp(self):
-#         self.app = create_app(TestConfig)
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
         
-#         self.clent =self.app.test_client(self)
+        self.clent =self.app.test_client(self)
         
-#         with self.app.app_context():
-#             db.init_app(self.app)
+        with self.app.app_context():
+            db.init_app(self.app)
             
-#             db.create_all()
+            db.create_all()
             
             
-#     def tearDown(self):
-#         with self.app.app_context():
-#                 db.session.remove()
-#                 db.drop_all()
+    def tearDown(self):
+        with self.app.app_context():
+                db.session.remove()
+                db.drop_all()
 
 
-# # Test runner
-# if __name__ =="__main__":
-#     unittest.main()
+# Test runner
+if __name__ =="__main__":
+    unittest.main()
 
         
