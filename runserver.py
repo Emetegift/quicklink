79ff4275cbe5bf1ab8b885b9798265a4d6b516a4
Emete Gift <emeteokeoghene@gmail.com>
2023-06-18 13:09:24 +0100

    Modified api

--- runserver.py
+++ runserver.py
@@ -1,14 +1,3 @@
-# from api import create_app
-# from api.config.config import config_dict
-
-
-# app = create_app(config=config_dict['dev'])
-
-
-# if __name__=="__main__":
-#     app.run()
-
-
 from api import create_app
 
 app = create_app()
