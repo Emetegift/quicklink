1ba66006a9919398accaa3107f21d8066a17eb47
Emete Gift <emeteokeoghene@gmail.com>
2023-06-21 14:14:07 +0100

    update api

--- api/__init__.py
+++ api/__init__.py
@@ -34,7 +34,7 @@ def create_app(config=config_dict['dev']):
     JWTManager(app)
     
     # Enable CORS for the application, allowing requests from http://localhost:3000
-    CORS(app, resources={r"/*": {"origins": "https://quicklink-fgakibner-emetegift.vercel.app"}})
+    CORS(app, resources={r"/*": {"origins": "https://quicklink-d8m7n36ma-emetegift.vercel.app"}})
     # CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
     # CORS(app, resupports_credentials=True)
     
