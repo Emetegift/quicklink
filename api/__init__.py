af231147759b9e75384f0d6810c93cbd6d603fb7
Emete Gift <emeteokeoghene@gmail.com>
2023-06-22 07:11:45 +0100

    Updated app

--- api/__init__.py
+++ api/__init__.py
@@ -34,8 +34,8 @@ def create_app(config=config_dict['dev']):
     JWTManager(app)
     
     # Enable CORS for the application, allowing requests from http://localhost:3000
-    # CORS(app, resources={r"/*": {"origins": "https://quicklink-gamma.vercel.app"}})
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    CORS(app, resources={r"/*": {"origins": "https://quicklink-gamma.vercel.app"}})
+    # CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
     # CORS(app, resupports_credentials=True)
     
     # Initialize the cache extension
